924ccbc012f427f874432c6dee0e27b5d56d4ba5
Jan De Wilde <jan@jandewilde.com>
2018-09-27 04:43:29 -0400

    #26 New and improved datatable, date is getting sorted alphabetically however

--- adminInbox.js
+++ adminInbox.js
@@ -1,29 +1,53 @@
-function employeeInboxRowTemplate(data) {
-	return `
-		<tr>
-			<th scope="row">${data.employeeName}</th>
-			<td>${data.mitId}</td>
-			<td>${data.fundingSource}</td>
-			<td>${data.submissionDate}</td>
-			<td>${data.academicYear}</td>
-			<td>${data.status}</td>
-		</tr>
-	`;
+
+
+/* Convert Unixtime to 'Month DD, YYYY' */
+function convertUnixTimeToHumanReadable(unixtime) {
+	let date = new Date(Number.parseInt(unixtime));
+	let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	let year = date.getFullYear();
+	let month = months[date.getMonth()];
+	let day = date.getDate();
+	return `${month} ${day}, ${year}`;
 };
 
 
-// Get Inbox JSON, iterate over template and inject as HTML
-$.getJSON('people.json', function(data) {
-	let employeeInboxRows = [];
-	let employeeInboxRowsString = "";
-	$.each( data.people, function (key, value) {
-	 employeeInboxRows.push(employeeInboxRowTemplate(value));
+/* Person Constructor */
+function Person(data){
+	person = {};
+	person.employeeName = data.employeeName;
+	person.mitId = data.mitId;
+	person.fundingSource = data.fundingSource;
+	person._submissionDate = data.submissionDate;
+	person.submissionDate = convertUnixTimeToHumanReadable(person._submissionDate);
+	person.academicYear = data.academicYear;
+	person.status = data.status;
+	return person;
+}
+
+
+// Initialize Admin Inbox
+function initTable(people) {
+	$('#inboxTable').DataTable({
+		data: people,
+		'columns': [
+			{data: 'employeeName'},
+			{data: 'mitId'},
+			{data: 'fundingSource'},
+			{data: 'submissionDate'},
+			{data: 'academicYear'},
+			{data: 'status'}
+		]
 	});
-
-	employeeInboxRowsString = employeeInboxRows.join('');
-
-	$('#inboxTable tbody').html(employeeInboxRowsString);
-});
+};
 
 
-$('#inboxTable').DataTable();
+/* IIFE to load employee JSON and create array of objects using Employee Constructor to pass to DataTable */
+var loadPeopleData = (function() {
+	let people = [];
+	$.getJSON('people.json', function(peopleData){
+		$.each(peopleData.people, function(key, value) {
+			people.push(Person(value));
+		});
+		initTable(people);
+	});
+})();

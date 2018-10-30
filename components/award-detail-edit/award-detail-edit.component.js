c251ea60505b8742e88ac436b944023c8cb0bc71
Jan De Wilde <jandw@mit.edu>
2018-10-29 22:03:56 -0400

    #26 award detail edit skeleton (copy of spouse detail edit)

---
+++ components/award-detail-edit/award-detail-edit.component.js
@@ -0,0 +1,57 @@
+angular.
+module('awardDetailEdit').
+component('awardDetailEdit', {
+  templateUrl: 'components/award-detail-edit/award-detail-edit.template.html',
+  bindings: {
+    $close: '&',
+    $dismiss: '&',
+    spouse: '=', //data gets passed in as an input on an spouse attribute on the custom element
+    employee: '=',
+    children: '='
+  },
+  controller: [ function AwardDetailEditController() {
+      let $ctrl = this;
+
+      $ctrl.$onChanges = function(changes) {
+        // Checkbox for toggeling mitid
+        if (typeof $ctrl.spouse === 'object') {
+          if ($ctrl.spouse.mitId) {
+            $ctrl.spouse.isMitIdPresent = true;
+          } else {
+            $ctrl.spouse.isMitIdPresent = false;
+          }
+
+          // angular requires date objects
+          if ($ctrl.spouse.appointmentStartDate) {
+            $ctrl.spouse._appointmentStartDate = new Date($ctrl.spouse.appointmentStartDate);
+          }
+          if ($ctrl.spouse.appointmentEndDate) {
+            $ctrl.spouse._appointmentEndDate = new Date($ctrl.spouse.appointmentEndDate);
+          }
+          if ($ctrl.spouse.selfEmploymentStartDate) {
+            $ctrl.spouse._selfEmploymentStartDate = new Date($ctrl.spouse.selfEmploymentStartDate);
+          }
+          if ($ctrl.spouse.schoolAdmissionDate) {
+            $ctrl.spouse._schoolAdmissionDate = new Date($ctrl.spouse.schoolAdmissionDate);
+          }
+          if ($ctrl.spouse.schoolExpectedGraduationDate) {
+            $ctrl.spouse._schoolExpectedGraduationDate = new Date($ctrl.spouse.schoolExpectedGraduationDate);
+          }
+        }
+      };
+
+      $ctrl.handleClose = function() {
+        $ctrl.$close({
+          result: $ctrl
+        });
+      };
+
+      $ctrl.handleDismiss = function() {
+        $ctrl.$dismiss({
+          reason: 'cancelled'
+        });
+      };
+
+    }
+  ]
+});

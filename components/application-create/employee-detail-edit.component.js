a66a62c4e1a19f0d8505abece32c7a025c2c5d8b
Jan De Wilde <jandw@mit.edu>
2018-12-19 12:33:52 -0500

    #26 verbiage updates draft award modal microcopy and award navigation

---
+++ components/application-create/employee-detail-edit.component.js
@@ -0,0 +1,39 @@
+angular.
+module('employeeDetailEdit').
+component('employeeDetailEdit', {
+  templateUrl: 'components/employee-detail-edit/employee-detail-edit.template.html',
+  bindings: {
+    $close: '&',
+    $dismiss: '&',
+    employee: '<' //data gets passed in as an input on an employee attribute on the custom element
+  },
+  controller: [function EmployeeDetailEditController() {
+      let $ctrl = this;
+
+      // Update marital title based upon marital status
+      $ctrl.updateSpouseMaritalTitle = function(){
+        if ($ctrl.employee.maritalStatus === 'married') {
+          $ctrl.employee.spouseMaritalTitle = 'spouse';
+        } else if ($ctrl.employee.maritalStatus === 'partnership') {
+          $ctrl.employee.spouseMaritalTitle = 'partner';
+        } else if ($ctrl.employee.maritalStatus === 'divorced') {
+          $ctrl.employee.spouseMaritalTitle = 'ex-spouse';
+        }
+      };
+      // close modal, return changes
+      $ctrl.handleClose = function() {
+        $ctrl.$close({
+          result: $ctrl.employee
+        });
+      };
+
+      // dismiss modal
+      $ctrl.handleDismiss = function() {
+        $ctrl.$dismiss({
+          reason: 'cancelled'
+        });
+      };
+
+    }
+  ]
+});
